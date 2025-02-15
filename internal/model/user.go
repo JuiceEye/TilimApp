e3d6a7dca2411e4c06e7ad3660a24d923e1d7321
JuiceEye <cool.faruh42@gmail.com>
2025-02-16 02:33:06 +0500

    add: Id to user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,6 +3,7 @@ package model
 import "time"
 
 type User struct {
+	Id               int64     `json:"id"`
 	Username         string    `json:"username"`
 	Password         string    `json:"password"`
 	Email            string    `json:"email"`
