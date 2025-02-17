bc44f9550b9d0d633257a918cdab3b6b03762d0f
Iminov_F <iminov.f@mybuh.kz>
2025-02-17 10:52:13 +0500

    change: decapitalize error messages

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -30,43 +30,43 @@ func GenerateJWT(w http.ResponseWriter, userID int) (string, error) {
 	return tokenString, err
 }
 
-// todo in middleware layer later
+//todo: add to the middleware layer later
 func VerifyJWT(r *http.Request) (int, error) {
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		return 0, fmt.Errorf("Authorization header is required")
+		return 0, fmt.Errorf("authorization header is required")
 	}
 
 	parts := strings.Split(authHeader, " ")
 	if len(parts) != 2 || parts[0] != "Bearer" {
-		return 0, fmt.Errorf("Invalid authorization header format")
+		return 0, fmt.Errorf("invalid authorization header format")
 	}
 
 	tokenString := parts[1]
 
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(config.Envs.JWTSecret), nil
 	})
 
 	if err != nil {
-		return 0, fmt.Errorf("Failed to parse token: %w", err)
+		return 0, fmt.Errorf("failed to parse token: %w", err)
 	}
 
 	if !token.Valid {
-		return 0, fmt.Errorf("Invalid token")
+		return 0, fmt.Errorf("invalid token")
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
 		userIDStr := claims["user_id"].(string)
 		userID, err := strconv.Atoi(userIDStr)
 		if err != nil {
-			return 0, fmt.Errorf("Invalid user_id in token")
+			return 0, fmt.Errorf("invalid user_id in token")
 		}
 		return userID, nil
 	}
 
-	return 0, fmt.Errorf("Invalid token claims")
+	return 0, fmt.Errorf("invalid token claims")
 }
