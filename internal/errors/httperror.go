5a33dedacbdf64a5032c2cc9b850b37d8b3dc692
JuiceEye <cool.faruh42@gmail.com>
2025-03-29 22:33:14 +0500

    add: custom HTTPError struct type

---
+++ internal/errors/httperror.go
@@ -0,0 +1,12 @@
+package errors
+
+import "fmt"
+
+type HTTPError struct {
+	Code    int
+	Message string
+}
+
+func (e *HTTPError) Error() string {
+	return fmt.Sprintf("HTTP %d: %s", e.Code, e.Message)
+}
