7efa035b0a36990f775e1a88d6bc2993f75aabf7
JuiceEye <cool.faruh42@gmail.com>
2025-04-14 23:04:10 +0500

    add: new profile service with GetProfile method

--- internal/service/profile_service.go
+++ internal/service/profile_service.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"errors"
+	"net/http"
+	"tilimauth/internal/model"
+	"tilimauth/internal/repository"
+)
+
+type ProfileService struct {
+	repository *repository.ProfileRepository
+}
+
+func NewProfileService(repository *repository.ProfileRepository) *ProfileService {
+	return &ProfileService{
+		repository: repository,
+	}
+}
+
+func (s *ProfileService) GetProfile(userID int64) (profile *model.Profile, status int, err error) {
+	profile, err = s.repository.GetProfileById(userID)
+	if errors.Is(err, repository.ErrNotFound) {
+		return nil, http.StatusNotFound, err
+	}
+
+	return profile, http.StatusOK, nil
+}
