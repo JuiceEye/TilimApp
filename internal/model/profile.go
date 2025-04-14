568a48e75681ab4b71a6bb6268502337e4311a5d
Iminov_F <iminov.f@mybuh.kz>
2025-04-14 17:57:18 +0500

    add: накидал по профилю

--- internal/model/profile.go
+++ internal/model/profile.go
@@ -1,1 +1,10 @@
 package model
+
+type Profile struct {
+	UserID      int64  `json:"id"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Image       string `json:"image"`
+}
