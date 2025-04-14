771192864f65f0d4d74b4488397502228d4c427f
JuiceEye <cool.faruh42@gmail.com>
2025-04-14 23:04:31 +0500

    add: new profile repository with getProfileByID method

---
+++ internal/repository/profile_repostiory.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"database/sql"
+	"tilimauth/internal/model"
+)
+
+type ProfileRepository struct {
+	db *sql.DB
+}
+
+func NewProfileRepo(db *sql.DB) *ProfileRepository {
+	return &ProfileRepository{
+		db: db,
+	}
+}
+
+func (r *ProfileRepository) GetProfileById(id int64) (*model.Profile, error) {
+	rows, err := r.db.Query("SELECT * FROM auth.users WHERE id = $1::INTEGER", id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	p := new(model.Profile)
+	for rows.Next() {
+		p, err = scanRowIntoProfiles(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if p.UserID == 0 {
+		return nil, ErrNotFound
+	}
+
+	return p, nil
+}
+
+func scanRowIntoProfiles(rows *sql.Rows) (*model.Profile, error) {
+	profile := new(model.Profile)
+
+	err := rows.Scan(
+		&profile.UserID,
+		&profile.Username,
+		&profile.RegistrationDate,
+		&profile.StreakDays,
+		&profile.XPPoints,
+		&profile.RegistrationDate,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return profile, nil
+}
