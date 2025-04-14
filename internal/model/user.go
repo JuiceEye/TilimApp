aabc3881822f5128335bdb655c404c0cfb90e159
Iminov_F <iminov.f@mybuh.kz>
2025-04-14 17:55:33 +0500

    test: remove json tags

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type User struct {
-	ID               int64     `json:"id"`
-	Username         string    `json:"username"`
-	Password         string    `json:"password"`
-	Email            string    `json:"email"`
-	PhoneNumber      string    `json:"phone_number"`
-	Image            string    `json:"image"`
-	RegistrationDate time.Time `json:"registration_date"`
+	ID               int64
+	Username         string
+	Password         string
+	Email            string
+	PhoneNumber      string
+	Image            string
+	RegistrationDate time.Time
 }
