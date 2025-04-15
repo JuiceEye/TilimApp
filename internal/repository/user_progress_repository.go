30302809c5e9c8bd4970d7771b26d50426b44deb
Iminov_F <iminov.f@mybuh.kz>
2025-04-15 17:26:26 +0500

    add: CreateUserProgress, rename from Profile to UserProgress

--- internal/repository/user_progress_repository.go
+++ internal/repository/user_progress_repository.go
@@ -3,13 +3,14 @@ package repository
 import (
 	"database/sql"
 	"tilimauth/internal/model"
+	"time"
 )
 
 type UserProgressRepository struct {
 	db *sql.DB
 }
 
-func NewProfileRepo(db *sql.DB) *UserProgressRepository {
+func NewUserProgressRepo(db *sql.DB) *UserProgressRepository {
 	return &UserProgressRepository{
 		db: db,
 	}
@@ -37,12 +38,38 @@ func (r *UserProgressRepository) GetUserProgressByUserID(UserID int64) (*model.U
 	return up, nil
 }
 
+func (r *UserProgressRepository) CreateUserProgress(UserID int64) (*model.UserProgress, error) {
+	up := &model.UserProgress{
+		UserID:                UserID,
+		StreakDays:            0,
+		XPPoints:              0,
+		WordsLearned:          0,
+		LessonsDone:           0,
+		LastLessonCompletedAt: nil,
+		CreatedAt:             time.Now(),
+		UpdatedAt:             time.Now(),
+	}
+
+	_, err := r.db.Exec(
+		`INSERT INTO tilim.user_progress
+		(user_id, streak_days, xp_points, words_learned, lessons_done, last_lesson_completed_at, created_at, updated_at)
+		VALUES ($1::INTEGER, $2::INTEGER, $3::INTEGER, $4::INTEGER, $5::INTEGER, $6::TIMESTAMPTZ, $7::TIMESTAMPTZ, $8::TIMESTAMPTZ)`,
+		up.UserID, up.StreakDays, up.XPPoints, up.WordsLearned, up.LessonsDone, up.LastLessonCompletedAt, up.CreatedAt, up.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return up, nil
+}
+
 func scanRowIntoUserProgress(rows *sql.Rows) (*model.UserProgress, error) {
 	userProgress := new(model.UserProgress)
 
 	err := rows.Scan(
 		&userProgress.UserID,
-		&userProgress.Streak,
+		&userProgress.StreakDays,
 		&userProgress.XPPoints,
 		&userProgress.WordsLearned,
 		&userProgress.LessonsDone,
