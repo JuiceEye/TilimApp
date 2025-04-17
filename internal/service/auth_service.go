575dd5619cbde857b90c891d6b93c251bab6e899
Iminov_F <iminov.f@mybuh.kz>
2025-04-17 17:51:21 +0500

    remove: prints

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -76,9 +76,6 @@ func (s *AuthService) Login(usernameOrEmail, password string) (*model.User, int,
 	}
 
 	if err := utils.ComparePassword(user.Password, password); err != nil {
-		fmt.Println("Ошибочка")
-		fmt.Println("Хеш из БД:", user.Password)
-		fmt.Println("Введённый пароль:", password)
 		return nil, http.StatusUnauthorized, fmt.Errorf("неверные учетные данные")
 	}
 
