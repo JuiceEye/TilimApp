1e09132ff0c535954042f92639d26cd91ad01dc9
Iminov_F <iminov.f@mybuh.kz>
2025-04-18 15:05:03 +0500

    change: GetModuleRequest to GetMainPageModuleRequest

--- internal/dto/request/module_request.go
+++ internal/dto/request/module_request.go
@@ -2,11 +2,11 @@ package request
 
 import "fmt"
 
-type GetModuleRequest struct {
+type GetMainPageModuleRequest struct {
 	ModuleID int64 `json:"module_id"`
 }
 
-func (req *GetModuleRequest) ValidateRequest() (err error) {
+func (req *GetMainPageModuleRequest) ValidateRequest() (err error) {
 	if req.ModuleID <= 0 {
 		return fmt.Errorf("module_id не может быть меньше 1")
 	}
