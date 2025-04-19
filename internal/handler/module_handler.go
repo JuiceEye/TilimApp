f534a49061daf6fca5eb146f03e3b9f1ab177a05
JuiceEye <cool.faruh42@gmail.com>
2025-04-19 14:47:54 +0500

    add: niche ne gotovo

--- internal/handler/module_handler.go
+++ internal/handler/module_handler.go
@@ -1,63 +1,63 @@
 package handler
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"strconv"
-// 	"tilimauth/internal/dto/request"
-// 	"tilimauth/internal/dto/response"
-// 	"tilimauth/internal/service"
-// 	"tilimauth/internal/utils"
-// )
-//
-// type ModuleHandler struct {
-// 	service *service.ModuleService
-// }
-//
-// func NewModuleHandler(service *service.ModuleService) *ModuleHandler {
-// 	return &ModuleHandler{
-// 		service: service,
-// 	}
-// }
-//
-// func (h *ModuleHandler) RegisterRoutes(router *http.ServeMux) {
-// 	router.HandleFunc("GET /main-page/module/{module_id}", h.handleGetMainPageModule)
-// }
-//
-// func (h *ModuleHandler) handleGetMainPageModule(w http.ResponseWriter, r *http.Request) {
-// 	payload := request.GetMainPageModuleRequest{}
-//
-// 	moduleID, err := strconv.ParseInt(r.PathValue("module_id"), 10, 64)
-// 	if err != nil {
-// 		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("module_id path param не найден"))
-// 		return
-// 	}
-// 	payload.ModuleID = moduleID
-//
-// 	if err := utils.ParseBodyAndValidate(r, &payload); err != nil {
-// 		utils.WriteError(w, http.StatusBadRequest, err)
-// 		return
-// 	}
-//
-// 	profile, err := h.service.GetModuleByID(payload.ModuleID)
-// 	if err != nil {
-// 		utils.WriteError(w, status, err)
-// 		return
-// 	}
-//
-// 	profileResponse := response.GetModuleResponse{
-// 		ModuleID:         profile.UserID,
-// 		Username:         profile.Username,
-// 		RegistrationDate: profile.RegistrationDate,
-// 		StreakDays:       profile.StreakDays,
-// 		XPPoints:         profile.XPPoints,
-// 		WordsLearned:     profile.WordsLearned,
-// 		LessonsDone:      profile.LessonsDone,
-// 	}
-//
-// 	err = utils.WriteJSONResponse(w, http.StatusOK, profileResponse)
-// 	if err != nil {
-// 		utils.WriteError(w, http.StatusInternalServerError, err)
-// 		return
-// 	}
-// }
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"tilimauth/internal/dto/request"
+	"tilimauth/internal/dto/response"
+	"tilimauth/internal/repository"
+	"tilimauth/internal/service"
+	"tilimauth/internal/utils"
+)
+
+type ModuleHandler struct {
+	service *service.ModuleService
+}
+
+func NewModuleHandler(service *service.ModuleService) *ModuleHandler {
+	return &ModuleHandler{
+		service: service,
+	}
+}
+
+func (h *ModuleHandler) RegisterRoutes(router *http.ServeMux) {
+	router.HandleFunc("GET /main-page/module/{module_id}", h.handleGetMainPageModule)
+}
+
+func (h *ModuleHandler) handleGetMainPageModule(w http.ResponseWriter, r *http.Request) {
+	payload := request.GetMainPageModuleRequest{}
+
+	moduleID, err := strconv.ParseInt(r.PathValue("module_id"), 10, 64)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("module_id path param не найден"))
+		return
+	}
+	payload.ModuleID = moduleID
+
+	if err := utils.ParseBodyAndValidate(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	module, err := h.service.GetMainPageModuleByID(payload.ModuleID) // ?? или надо просто модуль и вызывать много сервисов??
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			utils.WriteError(w, http.StatusNotFound, err)
+			return
+		}
+	}
+
+	moduleResponse := response.GetMainPageModuleResponse{
+		ID:       module.ID,
+		Title:    module.title,
+		Sections: module.sections,
+	}
+
+	err = utils.WriteJSONResponse(w, http.StatusOK, moduleResponse)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
---
+++ internal/service/module_service.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"errors"
+	"net/http"
+	"tilimauth/internal/model"
+	"tilimauth/internal/repository"
+)
+
+type ModuleService struct {
+	userRepository         *repository.UserRepository
+	userProgressRepository *repository.UserProgressRepository
+}
+
+func NewProfileService(
+	userRepository *repository.UserRepository,
+	userProgressRepository *repository.UserProgressRepository,
+) *ProfileService {
+	return &ProfileService{
+		userRepository:         userRepository,
+		userProgressRepository: userProgressRepository,
+	}
+}
+
+func (s *ProfileService) GetProfile(userID int64) (profile *model.Profile, status int, err error) {
+	user, err := s.userRepository.GetUserByID(userID)
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			return nil, http.StatusNotFound, err
+		}
+		return nil, http.StatusInternalServerError, err
+	}
+
+	userProgress, err := s.userProgressRepository.GetUserProgressByUserID(userID)
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			userProgress, err = s.userProgressRepository.CreateUserProgress(userID)
+			if err != nil {
+				return nil, http.StatusInternalServerError, err
+			}
+		} else {
+			return nil, http.StatusInternalServerError, err
+		}
+	}
+
+	profile = &model.Profile{
+		UserID:           user.ID,
+		Username:         user.Username,
+		RegistrationDate: user.RegistrationDate,
+		StreakDays:       userProgress.StreakDays,
+		XPPoints:         userProgress.XPPoints,
+		WordsLearned:     userProgress.WordsLearned,
+		LessonsDone:      userProgress.LessonsDone,
+	}
+
+	return profile, http.StatusOK, nil
+}
