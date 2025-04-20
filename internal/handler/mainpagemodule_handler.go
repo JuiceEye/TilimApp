466ad561b9d134689334a20cc5facb8cffe6d0ad
JuiceEye <cool.faruh42@gmail.com>
2025-04-20 21:38:00 +0500

    add: main page module handler

---
+++ internal/handler/mainpagemodule_handler.go
@@ -0,0 +1,58 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"tilimauth/internal/dto/request"
+	"tilimauth/internal/repository"
+	"tilimauth/internal/service"
+	"tilimauth/internal/utils"
+)
+
+type ModuleHandler struct {
+	service *service.MainPageModuleService
+}
+
+func NewModuleHandler(service *service.MainPageModuleService) *ModuleHandler {
+	return &ModuleHandler{
+		service: service,
+	}
+}
+
+func (h *ModuleHandler) RegisterRoutes(router *http.ServeMux) {
+	router.HandleFunc("GET /main-page/module/{module_id}", h.handleGetMainPageModule)
+}
+
+func (h *ModuleHandler) handleGetMainPageModule(w http.ResponseWriter, r *http.Request) {
+	payload := request.GetMainPageModuleRequest{}
+
+	moduleID, err := strconv.ParseInt(r.PathValue("module_id"), 10, 64)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("module_id path param не найден"))
+		return
+	}
+	payload.ModuleID = moduleID
+
+	if err := utils.ParseBodyAndValidate(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	module, err := h.service.GetMainPageModuleByID(payload.ModuleID)
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			utils.WriteError(w, http.StatusNotFound, err)
+			return
+		} else {
+			utils.WriteError(w, http.StatusInternalServerError, err)
+		}
+	}
+
+	err = utils.WriteJSONResponse(w, http.StatusOK, module)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
