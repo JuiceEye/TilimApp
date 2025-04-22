80c305dc3fa07b113c194be0ade0a7c9f19f5295
Iminov_F <iminov.f@mybuh.kz>
2025-04-22 15:00:33 +0500

    fix: answer struct

--- internal/model/exercise.go
+++ internal/model/exercise.go
@@ -1,9 +1,9 @@
 package model
 
 type Exercise struct {
-	ID           int64     `json:"id"`
-	Text         string    `json:"text"`
-	Image        string    `json:"image"`
-	QuestionText string    `json:"question_text"`
-	Answers      []Answers `json:"exercises"`
+	ID           int64    `json:"id"`
+	Text         string   `json:"text"`
+	Image        string   `json:"image"`
+	QuestionText string   `json:"question_text"`
+	Answers      []Answer `json:"exercises"`
 }
