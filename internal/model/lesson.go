29e130c7a75208dfd94cb491a20a76634592e638
Iminov_F <iminov.f@mybuh.kz>
2025-04-22 14:29:07 +0500

    add: exercises

--- internal/model/lesson.go
+++ internal/model/lesson.go
@@ -1,7 +1,8 @@
 package model
 
 type Lesson struct {
-	ID    int64  `json:"id"`
-	Title string `json:"title"`
-	XP    int64  `json:"xp"`
+	ID        int64       `json:"id"`
+	Title     string      `json:"title"`
+	XP        int64       `json:"xp"`
+	Exercises []Exercises `json:"exercises"`
 }
