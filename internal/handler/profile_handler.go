af337fdd05e946fb394d5294f12b70c0731203e0
mopsprivet <nastya.sitchenko@mail.ru>
2025-04-22 18:20:04 +0500

    change: user_progress_rep, profile_service, profile_handler: add xp to user logic

--- internal/handler/profile_handler.go
+++ internal/handler/profile_handler.go
@@ -22,6 +22,7 @@ func NewProfileHandler(service *service.ProfileService) *ProfileHandler {
 
 func (h *ProfileHandler) RegisterRoutes(router *http.ServeMux) {
 	router.HandleFunc("GET /profile/{user_id}", h.handleGetProfile)
+	router.HandleFunc("POST /profile/xp", h.handleAddXP)
 }
 
 func (h *ProfileHandler) handleGetProfile(w http.ResponseWriter, r *http.Request) {
@@ -60,4 +61,34 @@ func (h *ProfileHandler) handleGetProfile(w http.ResponseWriter, r *http.Request
 		utils.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
+}
+
+func (h *ProfileHandler) handleAddXP(w http.ResponseWriter, r *http.Request) {
+	var req request.LessonCompletedRequest
+	if err := utils.ParseBodyAndValidate(r, &req); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	status, err := h.service.AddXPPoints(req.UserID, req.XPPoints)
+	if err != nil {
+		utils.WriteError(w, status, err)
+		return
+	}
+
+	profile, status, err := h.service.GetProfile(req.UserID)
+	if err != nil {
+		utils.WriteError(w, status, err)
+		return
+	}
+	resp := response.GetProfileResponse{
+		UserID:           profile.UserID,
+		Username:         profile.Username,
+		RegistrationDate: profile.RegistrationDate,
+		StreakDays:       profile.StreakDays,
+		XPPoints:         profile.XPPoints,
+		WordsLearned:     profile.WordsLearned,
+		LessonsDone:      profile.LessonsDone,
+	}
+	utils.WriteJSONResponse(w, http.StatusOK, resp)
 }
