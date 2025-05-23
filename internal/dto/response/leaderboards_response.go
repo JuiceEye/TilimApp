ff97efe9704bec7b2a30eeac0a1c9d3e50a56cd5
Iminov_F <iminov.f@mybuh.kz>
2025-05-23 15:11:41 +0500

    fix: resp name

--- internal/dto/response/leaderboards_response.go
+++ internal/dto/response/leaderboards_response.go
@@ -1,6 +1,6 @@
 package response
 
-type GetLeaderboardsResponse struct {
+type GetLeaderboardsProfileResponse struct {
 	UserID   int64  `json:"user_id"`
 	Username string `json:"username"`
 	XPPoints int64  `json:"xp_points"`
