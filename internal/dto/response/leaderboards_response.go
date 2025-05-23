10c3de5c68a10a29d5404bb5d0c50245a0ab9496
Iminov_F <iminov.f@mybuh.kz>
2025-05-23 15:27:16 +0500

    add: dto transport

--- internal/dto/response/leaderboards_response.go
+++ internal/dto/response/leaderboards_response.go
@@ -1,8 +1,23 @@
 package response
 
+import "tilimauth/internal/model"
+
 type GetLeaderboardsProfileResponse struct {
 	UserID   int64  `json:"user_id"`
 	Username string `json:"username"`
 	XPPoints int64  `json:"xp_points"`
 	Image    string `json:"image"`
+}
+
+func ToProfileResponseList(profiles []*model.Profile) []GetLeaderboardsProfileResponse {
+	leaderboards := make([]GetLeaderboardsProfileResponse, 0, len(profiles))
+	for _, p := range profiles {
+		leaderboards = append(leaderboards, GetLeaderboardsProfileResponse{
+			UserID:   p.UserID,
+			Username: p.Username,
+			XPPoints: p.XPPoints,
+			Image:    p.Image,
+		})
+	}
+	return leaderboards
 }
