c13af65d30455159f31a74753a13bf7743a0d4a4
Iminov_F <iminov.f@mybuh.kz>
2025-05-23 15:07:54 +0500

    add: ldbrds response

--- internal/dto/response/leaderboards_response.go
+++ internal/dto/response/leaderboards_response.go
@@ -1,1 +1,8 @@
 package response
+
+type GetLeaderboardsResponse struct {
+	UserID   int64  `json:"user_id"`
+	Username string `json:"username"`
+	XPPoints int64  `json:"xp_points"`
+	Image    string `json:"image"`
+}
