126a59d5b781d0dccb6a85ed375267f233526c5f
Iminov_F <iminov.f@mybuh.kz>
2025-05-29 16:59:50 +0500

    remove: endpoint comment

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -27,7 +27,6 @@ func (h *AuthHandler) RegisterRoutes(router *http.ServeMux) {
 	router.HandleFunc("POST /register", h.handleRegister)
 	router.HandleFunc("POST /login", h.handleLogin)
 	router.HandleFunc("POST /refresh", h.handleRefreshToken) // это для обновления токенов эндпоинт
-	// router.HandleFunc("PUT /user/profile-picture", h.handleUpdateProfilePicture)
 }
 
 func (h *AuthHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
