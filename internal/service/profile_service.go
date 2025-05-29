02ac2e08f673e5e35c70e3983097d85b280bcbf9
Iminov_F <iminov.f@mybuh.kz>
2025-05-29 21:03:12 +0500

    add: UpdateUsername service function

--- internal/service/profile_service.go
+++ internal/service/profile_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"tilimauth/internal/model"
 	"tilimauth/internal/repository"
@@ -58,4 +59,26 @@ func (s *ProfileService) GetProfile(userID int64) (profile *model.Profile, statu
 
 func (s *ProfileService) UpdateProfilePicture(userID int64, image string) error {
 	return s.userRepository.UpdateUserImageByID(userID, image)
+}
+
+func (s *ProfileService) UpdateUsername(userID int64, newUsername string) error {
+	currentUser, err := s.userRepository.GetUserByID(userID)
+	if err != nil {
+		return err
+	}
+
+	if currentUser.Username == newUsername {
+		return errors.New("имя пользователя должно отличаться от старого")
+	}
+
+	otherUser, err := s.userRepository.GetUserByUsername(newUsername)
+	if err == nil {
+		if otherUser.ID != currentUser.ID {
+			return errors.New("имя пользователя уже занято")
+		}
+	} else if !errors.Is(err, repository.ErrNotFound) {
+		return fmt.Errorf("не удалось проверить имя пользователя: %w", err)
+	}
+
+	return s.userRepository.ChangeUserFields(userID, &model.User{Username: newUsername})
 }
