efe5453bda5c96e36ffc44156b527c58eedcdd43
Iminov_F <iminov.f@mybuh.kz>
2025-06-04 17:04:48 +0500

    add: time validation logic

--- internal/dto/request/subscription_request.go
+++ internal/dto/request/subscription_request.go
@@ -10,9 +10,13 @@ type SubscriptionPurchaseRequest struct {
 }
 
 func (req *SubscriptionPurchaseRequest) ValidateRequest() (err error) {
-	if req.ExpiresAt.Before(time.Now().UTC()) {
+	t := req.ExpiresAt
+
+	if t.Before(time.Now().UTC()) {
 		return fmt.Errorf("дата окончания подписки не может быть раньше текущей даты")
 	}
+
+	req.ExpiresAt = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 
 	return nil
 }
